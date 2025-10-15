1674ee6971bb124bcfd12feacb9debc8e7017314
emoutrey <E.L.Moutrey@gmail.com>
2025-10-15 17:18:06 -0500

    Maybe I should just use svgs

--- src/Dice.tsx
+++ src/Dice.tsx
@@ -13,11 +13,15 @@ function Dice(props: Props) {
     };
 
     return (
-        <>
-            <Triangle direction='up' />
+        <div className="flex flex-col">
+            <div className="w-20 h-40">
+                <Triangle direction='up' />
+            </div>
+            {/* this is where the picture of the die will go */}
+            {/* I want to make the shape with css rather than use svgs */}
             <button className="bg-blue-500" onClick={handleClick}>{diceLabel}</button>
             <Triangle direction='down' />
-        </>
+        </div>
     )
 }
 
--- src/DicePool.tsx
+++ src/DicePool.tsx
@@ -13,18 +13,13 @@ function DicePool() {
     {/* TODO: Make these pictures of dice or something, buttons and labels are just placeholders */}
     return (
         <div>
-            <Dice diceLabel="Add d4" diceFunction={() => setD4(totalDice < maxDice ? d4 + 1 : d4)} />
-            <Dice diceLabel="Add d6" diceFunction={() => setD6(totalDice < maxDice ? d6 + 1 : d6)} />
-            <Dice diceLabel="Add d8" diceFunction={() => setD8(totalDice < maxDice ? d8 + 1 : d8)} />
-            <Dice diceLabel="Add d10" diceFunction={() => setD10(totalDice < maxDice ? d10 + 1 : d10)} />
-            <Dice diceLabel="Add d12" diceFunction={() => setD12(totalDice < maxDice ? d12 + 1 : d12)} />
-            <br/>
-            <Dice diceLabel="Remove d4" diceFunction={() => setD4(d4 > 0 ? d4 - 1 : d4)} />
-            <Dice diceLabel="Remove d6" diceFunction={() => setD6(d6 > 0 ? d6 - 1 : d6)} />
-            <Dice diceLabel="Remove d8" diceFunction={() => setD8(d8 > 0 ? d8 - 1 : d8)} />
-            <Dice diceLabel="Remove d10" diceFunction={() => setD10(d10 > 0 ? d10 - 1 : d10)} />
-            <Dice diceLabel="Remove d12" diceFunction={() => setD12(d12 > 0 ? d12 - 1 : d12)} />
-            <br/>
+            <div className="flex flex-row">
+                <Dice diceLabel="Add d4" diceFunction={() => setD4(totalDice < maxDice ? d4 + 1 : d4)} />
+                <Dice diceLabel="Add d6" diceFunction={() => setD6(totalDice < maxDice ? d6 + 1 : d6)} />
+                <Dice diceLabel="Add d8" diceFunction={() => setD8(totalDice < maxDice ? d8 + 1 : d8)} />
+                <Dice diceLabel="Add d10" diceFunction={() => setD10(totalDice < maxDice ? d10 + 1 : d10)} />
+                <Dice diceLabel="Add d12" diceFunction={() => setD12(totalDice < maxDice ? d12 + 1 : d12)} />
+            </div>
             <button onClick={() => setTake2(!take2)}>{take2 ? 'Taking 2' : 'Taking 3'}</button>
             <br/>
             <div className="card">
