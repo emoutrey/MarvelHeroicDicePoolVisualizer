3a8ac62d6f28fea0d4f3f63050a2b8fdcd398aeb
emoutrey <E.L.Moutrey@gmail.com>
2025-10-20 17:35:58 -0500

    This gives me better control over the svgs anyway

--- src/Triangle.tsx
+++ src/Triangle.tsx
@@ -1,5 +1,5 @@
-import UpTriangle from "./assets/Pictogrammers-Material-Light-Chevron-up.svg";
-import DownTriangle from "./assets/Pictogrammers-Material-Light-Chevron-down.svg";
+//does single quotes vs double quotes matter here?
+//don't think it does but not sure how I feel about mixing them
 
 interface Props {
     direction: 'up' | 'down'
@@ -11,11 +11,11 @@ function Triangle(props: Props) {
     return (
         <>
             {direction == 'up' ? (
-                <UpTriangle />
+                <svg xmlns="http://www.w3.org/2000/svg" id="mdil-chevron-up" viewBox="0 0 24 24"><path d="M5.84,15.41L11.5,9.75L17.16,15.41L16.45,16.11L11.5,11.16L6.55,16.11L5.84,15.41Z" /></svg>
             ) : null}
 
             {direction == 'down' ? (
-                <DownTriangle />
+                <svg xmlns="http://www.w3.org/2000/svg" id="mdil-chevron-down" viewBox="0 0 24 24"><path d="M5.84,9.59L11.5,15.25L17.16,9.59L16.45,8.89L11.5,13.84L6.55,8.89L5.84,9.59Z" /></svg>
             ) : null}
         </>
     )
