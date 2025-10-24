2d6d8753aa4f167ce02999984e68704714df9dad
emoutrey <E.L.Moutrey@gmail.com>
2025-10-24 13:51:12 -0500

    I don't think this needed to be its own component

--- src/DiceController.tsx
+++ src/DiceController.tsx
@@ -2,14 +2,14 @@ import { useContext } from 'react';
 import { Context } from './App';
 import DieController from './DieController'
 import DicePool from './DicePool';
-import TakeSwitch from './TakeSwitch';
+import Switch from './Switch'
 
 //I don't like calling things on the frontend 'controllers'
 //But that is what it does technically
 //When I think of something better I'll change it again
 
 function DiceController() {
-    const {d4, setD4, d6, setD6, d8, setD8, d10, setD10, d12, setD12} = useContext<any>(Context);
+    const {d4, setD4, d6, setD6, d8, setD8, d10, setD10, d12, setD12, take2, setTake2} = useContext<any>(Context);
 
     //do I need to memoize this? Probably
     const totalDice = d4 + d6 + d8 + d10 + d12;
@@ -52,7 +52,7 @@ function DiceController() {
                     <DicePool />
                 </div>
                 <div className="pr-10">
-                    <TakeSwitch />
+                    <Switch checked={!take2} onChange={() => setTake2(!take2)} />
                 </div>
             </div>
         </div>
