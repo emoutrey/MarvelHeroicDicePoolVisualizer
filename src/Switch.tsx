bffddfe548ebb2e431b5dfd535ea83651f99a4f5
emoutrey <E.L.Moutrey@gmail.com>
2025-10-28 11:14:16 -0500

    Added switch labels

--- src/Switch.tsx
+++ src/Switch.tsx
@@ -20,11 +20,13 @@ const Switch = forwardRef((props: Props, forwardRef: Ref<HTMLInputElement>) => {
                 ref={forwardRef}
             />
             <div className="w-34 h-10 bg-gray-300 rounded-full relative cursor-pointer select-none">
-                <div className={`w-16 h-8 m-1 bg-white rounded-full absolute top-0 left-0 transition-transform transform ${props.checked ? "translate-x-16" : ""}`}>
-                    <div className="text-black leading-7">
-                        {/* TODO: put this underneath the button and have it slide over */}
-                        { checked ? "Take 3" : "Take 2" }
-                    </div>
+                <div className={`w-16 h-8 m-1 bg-white rounded-full absolute top-0 left-0 transition-transform transform ${props.checked ? "translate-x-16" : ""}`} />
+                {/* TODO: color these disabled or enabled when I decide on a color scheme */}
+                <div className="text-black leading-7 absolute top-1 left-3.5">
+                    Take 2
+                </div>
+                <div className="text-black leading-7 absolute top-1 left-19.5">
+                    Take 3
                 </div>
             </div>
         </label>
