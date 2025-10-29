41c9ed33051c6b41786ce75810a126c95490f019
emoutrey <E.L.Moutrey@gmail.com>
2025-10-29 16:47:21 -0500

    Minor cleanup

--- src/ProbabilityChart.tsx
+++ src/ProbabilityChart.tsx
@@ -6,23 +6,13 @@ import {
   CategoryScale,
   LinearScale,
   BarElement,
-  Title,
   Tooltip,
-  Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { generateResultRange, dataset, options } from './chartConfiguration';
 import { generatePermutationMap, take2Dice, take3Dice } from './chartFunctionality';
 
-//not all of these are necessary, will prune later
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
 
 function ProbabilityChart() {
     const {d4, d6, d8, d10, d12, take2} = useContext<ContextType>(Context);;
