37d2c37a683bf8509eb0de033acbf265fb1c7409
emoutrey <E.L.Moutrey@gmail.com>
2025-11-03 10:00:40 -0600

    Putting together modal content and configuration

--- src/NewUserModal.tsx
+++ src/NewUserModal.tsx
@@ -1,12 +1,36 @@
 import Modal from 'react-modal';
+import colors from 'tailwindcss/colors'
+
+const customStyles = {
+  content: {
+    backgroundColor: colors.neutral[800],
+    width: '480px',
+    minHeight: '80vh',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+function setCookie() {
+    //set a cookie to remember that the modal shouldn't open next time
+    return null;
+}
 
 function NewUserModal() {
 
     return (
         <Modal
-            isOpen={false}
+            isOpen={true}
+            style={customStyles}
+            onAfterClose={setCookie}
+            contentLabel={'aria'}
         >
-            Test
+            
+
+            
+            
+            
         </Modal>
     )
 }
