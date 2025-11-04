38a480e7fd8446d5a5577c96c4ce2e6b8dd86067
emoutrey <E.L.Moutrey@gmail.com>
2025-11-04 17:17:42 -0600

    Fixing up modal

--- src/NewUserModal.tsx
+++ src/NewUserModal.tsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import Modal from 'react-modal';
 import colors from 'tailwindcss/colors'
 
-//is there a way to use Tailwind here?
 const customStyles = {
   content: {
     backgroundColor: colors.neutral[800],
-    width: '50vw',
     minWidth: '480px',
+    maxWidth: '50vw',
     height: '80vh',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
+    padding: '0',
   },
 };
 
@@ -23,31 +23,38 @@ function setCookie() {
 function NewUserModal() {
     const [open, setOpen] = useState(true);
 
+    //TODO fix "React-Modal: Cannot register modal instance that's already open"
+    Modal.setAppElement('#root');
+
     // TODO:
     // Set cookie when modal is closed and don't open on page load if cookie is set
     // Put icon in top right to re-open modal if requested
-    // Make bottom div Fixed
     // Style modal content (title, etc)
-    // Figure out wonkiness with modal height and button text centering
     
+    //TODO: Modal doesn't close on overlay click
     return (
         <Modal
             isOpen={open}
             style={customStyles}
             onAfterClose={setCookie}
             contentLabel={'aria'}
+            shouldCloseOnOverlayClick={true}
         >
-            <div className='mb-4'>Welcome to Cortex Plus Dice Pool Visualizer!</div>
-            <div className='mb-4'>Cortex Plus is the underpinning of multiple tabletop systems, including <i>Leverage: The Roleplaying Game</i>, <i>The Firefly Role-Playing Game</i>, <i>The Smallville Roleplaying Game</i>, and <i>Marvel Heroic Roleplaying</i>.</div>
-            <div className='mb-4'>To make a roll in a Cortex Plus system, you assemble your dice pool from your character's abilities, and then roll all of them. You then select either the top two or top three results, and add them together for your Result.</div>
-            <div className='mb-4'>In addition, a remaining die -- one which did not contribute to the Result -- is selected as the Effect Die for the roll. Any rolls of a one additionally generate Opportunities for your opponent.</div>
-            <div className='mb-4'>As it is not straightforward to figure out what to expect from a dice pool during play, I created this tool to visualize average Results, Effect Dice, and Opportunity generation for a given roll.</div>
-            <div className='mb-4'>I hope this tool helps you as it did me! Happy gaming!</div>
-            <div className='mb-4'>To view this modal again after closing it, click the (put an icon here) icon in the top right.</div>
-            <div className='flex justify-center'>
-                <button className='border rounded-xl border-neutral-600 hover:border-neutral-300 transition-colors duration-250 px-4 py-2 active:scale-95' onClick={() => setOpen(false)}>
-                    <span className=''>Close</span>
-                </button>
+            <div className='h-4/5 overflow-y-auto p-5'>
+                <h1 className='mb-4'>Cortex Plus Dice Pool Visualizer</h1>
+                <p className='mb-4'>Cortex Plus is the underpinning of multiple tabletop systems, including <i>The Firefly Role-Playing Game</i> and <i>Marvel Heroic Roleplaying</i>.</p>
+                <p className='mb-4'>To perform a roll in a Cortex Plus system, you assemble your dice pool from your character's abilities, and then roll all of the dice at once. You then select either two or three of these dice, and add them together for your Result.</p>
+                <p className='mb-4'>In addition, a remaining die -- one which did not contribute to the Result -- is selected as the Effect Die for the roll. Any rolls of a one additionally generate Opportunities for your opponent, and cannot be used for either your Result or Effect Die.</p>
+                <p className='mb-4'>Since it's not necessarily straightforward to figure out what to expect from a dice pool during play, I created this tool to visualize average Results, Effect Dice, and Opportunity generation for a given set of rolls.</p>
+                <p className='mb-4'>I hope this tool helps you as it did me! Happy gaming!</p>
+                <p className='mb-4'>To view this window again after closing it, click the (put an icon here) icon in the top right.</p>
+            </div>
+            <div className='h-1/5'>
+                <div className='h-full flex justify-center items-center'>
+                    <button className='border rounded-xl border-neutral-600 hover:border-neutral-300 transition-colors duration-250 px-4 py-2 active:scale-95' onClick={() => setOpen(false)}>
+                        <span className=''>Close</span>
+                    </button>
+                </div>
             </div>
         </Modal>
     )
