bc46c1923f885fcc098f73c37ae02d34fce2aa59
emoutrey <E.L.Moutrey@gmail.com>
2025-11-20 08:36:59 -0600

    I think I need to use GHA for this

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://emoutrey.github.io/CortexPlusDicePoolVisualizer/",
+  base: "/CortexPlusDicePoolVisualizer/",
 })
