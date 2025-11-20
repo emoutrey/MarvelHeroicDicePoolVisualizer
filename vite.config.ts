37259115f757413eeacfb945b15fa27fca7e313a
emoutrey <E.L.Moutrey@gmail.com>
2025-11-20 08:26:36 -0600

    config url

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://emoutrey.github.io/CortexPlusDicePoolVisualizer/",
 })
